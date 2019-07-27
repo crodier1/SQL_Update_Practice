9478196813c2a8838b9f9960bdc1f770d456e01b
Christian Rodier <crodier@protonmail.com>
2019-07-27 09:24:44 -0600

    1st commit

--- SQL Update Practice/Program.cs
+++ SQL Update Practice/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace SQL_Update_Practice
 {
     class Program
     {
+        static string connString = @"Data Source=DESKTOP-3BDGT3N;Initial Catalog=AddressBook;Integrated Security=True";
+        static SqlConnection conn;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Id Num");
+            string idQuestion = Console.ReadLine();
+            int id = Convert.ToInt32(idQuestion);
+
+            Console.WriteLine("Enter new company name");
+
+            string newCompanyName = Console.ReadLine();
+
+
+
+            string updateStatement = $@"update BizContacts 
+                                        set company = '{newCompanyName}'
+                                        where id = {id}";
+
+            //Console.WriteLine(updateStatement);
+
+
+            using (conn = new SqlConnection(connString))
+            {
+                try
+                {
+
+                    conn.Open();
+
+                    SqlCommand comm = new SqlCommand(updateStatement, conn);
+
+                    comm.ExecuteNonQuery();
+
+                    conn.Close();
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+
+
+
+
+
+
+            Console.Read();
         }
     }
 }
